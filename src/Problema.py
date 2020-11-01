b9ebf58271c1465f5c3b2108ddf0bbbeec8afa48
Iago <iago.garcia.suarez@gmail.com>
2020-11-01 11:35:50 +0100

    Terminada implementación de la entrega 2 excepto escritura en json.
    Implementados los artefactos software requeridos así como la preparación para alguna función de la siguiente entrega. Falta guardar en json lo que pide el punto 5 y comentar correctamente los archivos.

--- src/Problema.py
+++ src/Problema.py
@@ -3,8 +3,11 @@ from Sucesores import Sucesores
 from Estado import Estado
 
 class Problema():
-    def __init__(self, path, start):
+    def __init__(self, path, start, end):
         self.laberinto = Laberinto(True, path)
         self.sucesores = Sucesores()
         self.estado = Estado(self.laberinto[start[0]][start[1]])
+        self.end = end
         
+    def objetivo(self, id):
+        return id == self.end
