20e676b3af9e052c9d1a10ad693be154ec026671
Iago <iago.garcia.suarez@gmail.com>
2020-11-10 16:17:01 +0100

    Adaptadas las clases Launcher.py y Problema.py para el trabajo sobre el json del problema en vez del json del laberinto. Arreglados algunos errores de formateado.

--- src/Launcher.py
+++ src/Launcher.py
@@ -1,6 +1,6 @@
 import sys
 import os
-from Laberinto import Laberinto
+from Problema import Problema
 
 asciiTitle = '''
 
@@ -23,26 +23,28 @@ def menuStart():
     print('Bienvenid@, seleccione una opción para iniciar el programa:')
     while 1:
         try:
-            utilizarJson = int(input('1. Construir el laberinto a partir de un archivo .json.\n2. Generar el laberinto y guardarlo en un archivo .json\n'))
+            utilizarJson = int(input('1. Generar un problema a partir de un archivo .json.\n2. Crear el problema y guardarlo en un archivo .json\n'))
             if utilizarJson == 1:
                 JSON = True
                 while True:
-                    path = input("Introduzca el nombre del archivo json donde se almacena el laberinto:\n ")
+                    path = input("Introduzca el nombre del archivo json donde se almacena el problema (sin escribir la extensión .json):\n ")
                     if (os.path.exists(path)):
                         break
                     else:
-                        print("La ruta del fichero que ha introducido no es válida.")
-                Laberinto(JSON, path).drawMaze()
+                        print("La ruta del fichero que ha introducido no es válida. Recuerde escribirla sin la extensión .json.")
+                Problema(JSON, path)
                 break
+
             if utilizarJson == 2:
                 JSON = False
-                path = input("Introduzca el nombre del archivo json donde se almacenará el laberinto:\n ")
+                path = input("Introduzca el nombre del archivo json donde se almacenará el problema (sin escribir la extensión .json):\n ")
                 SIZE = (int(input('Introduzca el número de filas: ')), int(input('Introduzca el número de columnas: ')))
-                Laberinto(JSON, path, SIZE)
-                Laberinto(True, path).drawMaze()
+                Problema(JSON, path, SIZE)
                 break
+
             if utilizarJson not in [1, 2]: 
                 print('Error al introducir la opción, por favor escriba una de las opciones válidas.')
+
         except ValueError:
             print('Error al introducir la opción, por favor escriba una de las opciones válidas.')
     
--- src/Problema.py
+++ src/Problema.py
@@ -2,12 +2,28 @@ from Laberinto import Laberinto
 from Sucesores import Sucesores
 from Estado import Estado
 
+import json
+
 class Problema():
-    def __init__(self, path, start, end):
-        self.laberinto = Laberinto(True, path)
-        self.sucesores = Sucesores()
-        self.estado = Estado(self.laberinto[start[0]][start[1]])
-        self.end = end
+    def __init__(self, JSON, path, size = None):
+        self.pathProb = path + '.json'
+        if JSON:
+            self.problem_data = json.load(open(self.pathProb))
+            self.laberinto = Laberinto(True, self.problem_data["MAZE"])
+            self.start = eval(self.problem_data["INITIAL"])
+            self.estado = Estado(self.laberinto[start[0]][start[1]])
+            self.objective = eval(self.problem_data["OBJECTIVE"])
+        else:
+            self.pathMaze = path + '_maze.json'
+            self.laberinto = Laberinto(False, self.pathMaze, size)
+            self.start = (0, 0)
+            self.objective = (size[0]-1, size[1]-1)
         
+        self.sucesores = Sucesores()
+
     def objetivo(self, id):
-        return id == self.end
+        return id == self.objective
+'''
+    def saveJSON(self):
+        #Implementar aquí la función saveJSON similar a la que hay en Laberinto.py pero adaptada a Problema.py
+'''
